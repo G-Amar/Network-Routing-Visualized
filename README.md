43cc55b4715d990f2c8923d333fb206836b126ff
G-Amar <amargupta3327@gmail.com>
2023-03-15 19:52:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CPS706-Routing
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-4twvnx)
---
+++ index.js
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+hello;
+Amar;
