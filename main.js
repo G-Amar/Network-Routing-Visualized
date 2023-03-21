8fb614a0fe5eb807b4da2fa2eadbd0c171e533ed
Ammar Chaudhry <ammarsc02@gmail.com>
2023-03-21 19:08:10 -0400

    added features to add nodes and edges

--- main.js
+++ main.js
@@ -20,4 +20,19 @@ let addNode = event => {
     )
 }
 
+let removeNode = event => {
+    let input = document.getElementById("removeNode").value;
+    var toRemove = cy.$(`#${input}`)
+    cy.remove(toRemove);
+}
+
+let addEdge = event => {
+    let node1 = document.getElementById("edge1").value;
+    let node2 = document.getElementById("edge2").value;
+    let weight  = document.getElementById("weight").value;
+    cy.add({ group: 'edges', data: { id: `${node1}${node2}`, source: `${node1}`, target: `${node2}`, weight: `${weight}` }})
+}
+
+document.getElementById('addEdgeBtn').addEventListener("click", addEdge);
+document.getElementById('removeNodeBtn').addEventListener('click', removeNode);
 document.getElementById('addNodeBtn').addEventListener('click', addNode);
